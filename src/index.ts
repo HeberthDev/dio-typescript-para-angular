2213bf6d844ca6f0a1e443a49146cbb7b32379d5
HeberthDev <heberthdeveloper@gmail.com>
2024-01-10 08:39:13 -0300

    Funções Async

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ function callToPhone(phone: number | string): number | string {
     return phone;
 }
 
+async function getDatabase(id:number): Promise<string> {
+    return "felipe";
+}
+
 let soma: number = addNumber(4, 7);
 
 console.log(soma);
