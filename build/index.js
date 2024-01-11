219f7176cce704f95e4d7439de27609b1649ea3f
HeberthDev <heberthdeveloper@gmail.com>
2024-01-11 07:57:03 -0300

    Utilizando Generics na prática

--- build/index.js
+++ build/index.js
@@ -5,5 +5,6 @@ function concatArray(...itens) {
 }
 const numArray = concatArray([1, 5], [3]);
 const stgArray = concatArray(["felipe", "goku"], ["vegeta"]);
+numArray.push("saitama");
 console.log(numArray);
 console.log(stgArray);
