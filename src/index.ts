9b57fe5b03ca4fdf6e39300509328677d01ae2ec
HeberthDev <heberthdeveloper@gmail.com>
2024-01-11 07:42:47 -0300

    SubClasses

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@
 // data modifiers
 // public - Conseque acessar em qualquer lugar. Valor padrão.
 // private - Só pode ser acessado dentro da própria classe.
-// protected - Só é acessado na própria classe e quem herda as suas características (subclasses) (implements);
+// protected - Só é acessado na própria classe e quem herda as suas características (subclasses) (extends);
 class Character {
-    private name?: string;
+    protected name?: string;
     readonly stregth: number;
     skill: number;
 
@@ -20,6 +20,23 @@ class Character {
     }
 }
 
+//Character: superclass
+//Magician: subclass
+class Magician extends Character {
+    magicPoints: number;
+
+    constructor(
+        name: string, 
+        stregth: number, 
+        skill: number,
+        magicPoints: number,
+    ){
+        super(name, stregth, skill);
+        this.magicPoints = magicPoints;
+    }
+}
+
 const p1 = new Character("Ryu", 10, 98);
+const p2 = new Magician("Mago", 9, 30, 100);
 console.log(p1);
 p1.attack();
