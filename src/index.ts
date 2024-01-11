957d8f92ca104944365e71c8a394d2b4d994c09f
HeberthDev <heberthdeveloper@gmail.com>
2024-01-11 07:22:57 -0300

    Trabalhando com Classes

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+//classes
+class Character {
+    name?: string;
+    stregth: number;
+    skill: number;
+
+    constructor(stregth: number, skill: number) {
+        this.stregth = stregth;
+        this.skill = skill;
+    }
+
+    attack(): void {
+        console.log(`Attack with ${this.stregth} points`);
+    }
+}
+
+const p1 = new Character(10, 98);
+console.log(p1);
+p1.attack();
