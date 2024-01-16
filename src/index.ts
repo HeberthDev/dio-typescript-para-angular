2c930cee490be78210844029fb5cb6faa067e6a1
HeberthDev <heberthdeveloper@gmail.com>
2024-01-16 08:55:57 -0300

    Atribute Decorator

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,34 @@ function apiVersion(version: string) {
     };
 }
 
-@apiVersion("1.10")
-class Api {}
+function minLength(length: number) {
+    return (target: any, key: string) => {
+        let _value = target[key];
 
-const api = new Api();
-console.log(api.__version);
+        const getter = () => "[play]" + _value;
+        const setter = (value: string) => {
+            if (value.length < length) {
+                throw new Error(`Tamanho menor do que ${length}.`);
+            } else {
+                _value = value;
+            }
+        };
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+        });
+    };
+}
+
+class Api {
+    @minLength(3)
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const api = new Api("usa");
+console.log(api.name);
